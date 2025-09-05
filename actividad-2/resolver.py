af3b18656d72a3216f9b6083fd9e6bb9440e1d46
valgguk <valrj.college.study@gmail.com>
2025-09-04 21:48:40 -0400

    feat(resolver): implement DNS resolver with socket handling

---
+++ actividad-2/resolver.py
@@ -0,0 +1,54 @@
+import socket
+import datetime
+import json
+import sys
+
+
+
+if __name__ == "__main__":
+    # Manejo archivos Json
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    config_file = sys.argv[1]
+    with open(config_file, "r") as f:
+        config = json.load(f)
+    nombre_usuario = config.get("nombre", "Desconocido")
+
+    # definimos el tamaño del buffer de recepción y la secuencia de fin de mensaje
+    buff_size = 5000 #????
+    end_of_message = "\n"
+
+    new_socket_address = ('localhost', 8000)
+
+    print('Creando socket - DNS Resolver')
+    # armamos el socket
+    # los parámetros que recibe el socket indican el tipo de conexión
+    # socket.SOCK_STREAM = socket NO orientado a conexión
+    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # le indicamos al server socket que debe atender peticiones en la dirección address
+    # para ello usamos bind
+    socket.bind(new_socket_address)
+
+    # luego con listen (función de sockets de python) le decimos que puede
+    # tener hasta 3 peticiones de conexión encoladas
+    # si recibiera una 4ta petición de conexión la va a rechazar
+    socket.listen(3)
+
+    # nos quedamos esperando a que llegue una petición de conexión
+    print('DNS Resolver http://localhost:8000 esperando clientes...')
+    print()
+    while True:
+
+        request_test, addr = socket.recvfrom(buff_size)
+        print("Mensaje recibido: ", request_test)
+        print("Dirección del cliente: ", addr)
+
+        # cerramos la conexión
+        # notar que la dirección que se imprime indica un número de puerto distinto al 5000
+        print("\n\n") 
+        print(f"conexión con {addr} ha sido cerrada")
+
+        # seguimos esperando por si llegan otras conexiones
+
+
